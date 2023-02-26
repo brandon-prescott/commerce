664aa9b159f93e972ea1d4bcff39fcae9bf36617
Brandon <brandon@ubuntu.home>
2023-02-26 20:49:19 +0000

    created category and listing class

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Category, Listing # Database tables
 
 # Register your models here.
+# This allows a superuser to interact with the database on the admin site
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Listing)
--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,22 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category_name
+
+class Listing(models.Model):
+    title = models.CharField(max_length=40)
+    description = models.CharField(max_length=400)
+    price = models.FloatField()
+    image_url = models.CharField(max_length=1500)
+    is_active = models.BooleanField(default=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user") # One user can be associated with many listings
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category") # One category can be associated with many listings
+    time_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
