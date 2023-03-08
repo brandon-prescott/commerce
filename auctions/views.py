f2ab6d237979e3f8e906a551859772b30d3670de
Brandon <brandon@ubuntu.home>
2023-03-08 22:34:00 +0000

    created new listing form and page

--- auctions/views.py
+++ auctions/views.py
@@ -1,11 +1,49 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
 
-from .models import User
+from .models import User, Category, Listing
 
+
+class CreateListingForm(forms.ModelForm):
+    # Creates form for Listing model
+    class Meta:
+        model = Listing
+        fields = ["title", "description", "image_url", "price", "category"]
+        labels = {
+            "title": "Title",
+            "description": "Description",
+            "image_url": "Image URL",
+            "price": "Price",
+            "category": "Category"
+        }
+        widgets = {
+            "title": forms.TextInput(attrs={
+            "placeholder": "Enter a title for this item",
+            "class": "form-control"
+            }),
+            "description": forms.Textarea(attrs={
+            "placeholder": "Describe this item in more detail",
+            "class": "form-control"
+            }),
+            "image_url": forms.URLInput(attrs={
+            "placeholder": "A URL can be used to display an image",
+            "class": "form-control"
+            }),
+            "price": forms.NumberInput(attrs={
+            "placeholder": "Enter a starting bid",
+            "min": 0.01,
+            "max": 1000000000,
+            "class": "form-control"
+            }),
+            "category": forms.Select(attrs={
+            "class": "form-control"
+            })
+        }
 
 def index(request):
     return render(request, "auctions/index.html")
@@ -61,3 +99,14 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+@login_required(login_url="login")
+def create_listing(request):
+    if request.method == "POST":
+        return render(request, "auctions/index.html")
+
+    else:
+        return render(request, "auctions/create.html", {
+            "form": CreateListingForm
+        })
